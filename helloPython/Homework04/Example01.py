5eccaea5b8ae909f746585c61d412158f5cc30aa
Denis_Gerasimov <Pythonchic@gmail.com>
2022-12-12 20:33:03 +0300

    finish Homework04/Example01

--- helloPython/Homework04/Example01.py
+++ helloPython/Homework04/Example01.py
@@ -1,1 +1,21 @@
 
+# Вычислить число Пи c заданной точностью d
+
+# *Пример:* 
+
+# - при d = 0.001, π = 3.141
+# - при d = 0.0001, π = 3.1415 
+
+import math
+
+def roundingPi(x):
+    return round(math.pi, x)
+
+def findCur(x):
+    return len(x)-2
+
+
+cur = input('Введите точность в формате "0.001": ')
+fin = findCur(cur)
+print(f'- при d = {cur}, π = {roundingPi(fin)}')
+
