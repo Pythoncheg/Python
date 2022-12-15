637228f5d102ba81cbbd08cf23400f79c22210f5
Denis_Gerasimov <Pythonchic@gmail.com>
2022-12-15 20:31:30 +0300

    fin HW04

--- helloPython/Homework04/Example05.py
+++ helloPython/Homework04/Example05.py
@@ -7,3 +7,21 @@
 # 4*x**2 + 1*x + 4
 # Результирующий файл:
 # 6*x**2 + 5*x + 9
+
+import sympy
+
+with open('hellopython/homework04/file1.txt', 'r') as f1:
+    first_polinom = f1.read()
+    print(f'Первый полином: {first_polinom}')
+
+with open('hellopython/homework04/file2.txt', 'r') as f2:
+    second_polinom = f2.read()
+    print(f'Второй полином: {second_polinom}')
+
+sum_polinoms = sympy.simplify(first_polinom + '+' + second_polinom)
+sum_polinoms = str(sum_polinoms)
+
+print(f'Сумма полиномов = {sum_polinoms}')
+
+with open('hellopython/homework04/sum.txt', 'w') as wr:
+    wr.write(sum_polinoms)
