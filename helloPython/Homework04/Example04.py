2343b0d43fd9aa861dae2458c7c9e58849c08a93
Denis_Gerasimov <Pythonchic@gmail.com>
2022-12-15 20:12:19 +0300

    Fin Ex04 in HW04

--- helloPython/Homework04/Example04.py
+++ helloPython/Homework04/Example04.py
@@ -4,12 +4,30 @@
 
 # Пример:
 
-# k=2 => 2*x*2 + 4*x + 5 = 0 или x2 + 5 = 0 или 10*x*2 = 0
-# k=3 => 2*x*3 + 4*x*2 + 4*x + 5 = 0 5'. Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
-# В file1.txt :
-# 2*x**2 + 4*x + 5
-# В file2.txt:
-# 4*x**2 + 1*x + 4
-# Результирующий файл:
-# 6*x**2 + 5*x + 9
+# k=2 => 2*x^2 + 4*x + 5 = 0 или x^2 + 5 = 0 или 10*x^2 = 0
+# k=3 => 2*x^3 + 4*x^2 + 4*x + 5 = 0 
+# 
+import sympy
+from random import randint as r
+
+def create_polinom(k: int, simple: bool =  True):
+    polinom = ' '
+    for i in range(k, -1, -1):
+        polinom += f'{r(0,2)}*x^{i}+'
+        if i==0:
+            polinom+=f'{r(0,100)}*x^{i}'
+    if simple:
+        return str(sympy.simplify(polinom))
+    else:
+        return str(polinom)
+
+def create_file(polinom, filename: str = 'new'):
+    with open(f'helloPython/Homework04/{filename}.txt', 'w') as f:
+        f.write(polinom)       
+
+
+k = int(input("Введите коэффициент K: "))
+print(create_polinom(k))
+create_file(create_polinom(k), 'file2')
+
 
